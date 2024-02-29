339af7ace0169ca90b8ba1f2378fbb4eef6d4266
Bruno Noveli <brunimzuppwolf@gmail.com>
2024-02-29 13:26:25 -0300

    Initial Commit

---
+++ app/src/main/java/com/novelitech/flowkotlinone/MainViewModel.kt
@@ -0,0 +1,24 @@
+package com.novelitech.flowkotlinone
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+
+class MainViewModel : ViewModel() {
+
+    val countDownFlow = flow<Int> {
+
+        val startingValue = 10
+        var currentValue = startingValue
+
+        emit(currentValue)
+
+        while(currentValue > 0) {
+            delay(1000L)
+
+            currentValue--
+
+            emit(currentValue)
+        }
+    }
+}
